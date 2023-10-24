82b60fb35a78a06e0171321b95af78aa22331a93
DiegoPachon <79177336+DiegoPachon@users.noreply.github.com>
2023-10-24 11:19:14 -0500

    Probando FASTAPI-GET

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 #instancia de fastapi
 #uvicorn main:app --reload --port 5000 -host 0.0.0.0 
 #con el comando de arriba estara disponible la app en los dispositivos de la red
@@ -8,8 +9,24 @@ app.title = "Mi aplicacion con FastAPI"
 
 #Para cambiar la version de la aplicacion
 app.version = "0.0.1"
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
 #endpoint
 #los tags nos permite agrupar las rutas de la aplicacion
 @app.get("/", tags=['home'])
 def read_root():
-    return {"Hello": "World"}
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
